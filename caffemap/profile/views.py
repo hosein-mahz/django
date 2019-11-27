76ed01f09b600f77c31fa751294cd792e5229d99
mohammad hossain zarei <hoeinmhz986@gmail.com>
2019-11-27 15:31:36 +0330

    update profile view

--- caffemap/profile/views.py
+++ caffemap/profile/views.py
@@ -15,20 +15,14 @@ def getAll(request):
     # 
     g = serializers.serialize("json", Profile.objects.all())
     data = loads(g)
-    o = []
     for x in data:
         x['fields']['id']=x['pk']
-        for y in x['fields']['caffe']:
-            a = loads(serializers.serialize("json", [Caffe.objects.get(id=y)]))
-            o.append(a[0]['fields'])
-        x['fields']['caffe']=o
-        o = []
-        # x['fields'].append(o)
-        # 
-        # FIXME:
-        # 
-        # Profile.objects.get(id=1).caffe_set.all()
-        # 
+        l = serializers.serialize("json", Profile.objects.get(pk=x['pk']).caffe_set.all() )
+        l = loads(l)
+        x['fields']['caffe'] = []
+        for y in l:
+            x['fields']['caffe'].append(y['fields'])
+        
     return JsonResponse(data, safe=False)
 
 def getSingle(request, __id):
@@ -36,3 +30,16 @@ def getSingle(request, __id):
     data = loads(g)
     data[0]['fields']['id']=data[0]['pk']
     return JsonResponse(data[0]['fields'], safe=False)
+
+def create(request):
+    if request.method == 'POST':
+        try:
+            data = loads(request.body)
+            if data['name'] == '' or hasattr(data, 'name') :
+                return JsonResponse({'message':'unsuccessful createing'}, safe=False)
+            _Profile = Profile(name = data['name'], lastname = data['lastname'], username = data['username'], password = data['password'], email = data['email'])
+            _Profile.save()
+            return JsonResponse({'message':'successful create new Profile'}, safe=False)
+        except:
+            return JsonResponse({'message':'unsuccessful createing'}, safe=False)
+    return JsonResponse({'message': 'most use POST method'}, safe=False)
